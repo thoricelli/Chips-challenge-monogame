85050b60958db65153cb90df2c05891c653a408e
thoricelli <roanheylen@hotmail.com>
2023-11-02 21:49:25 +0100

    Rename to CHIP.

--- Chips-challenge-monogame/CHIP.cs
+++ Chips-challenge-monogame/CHIP.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Chips_challenge_monogame
 {
-    public class Game1 : Game
+    public class CHIP : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public CHIP()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Chips-challenge-monogame/Program.cs
+++ Chips-challenge-monogame/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new Chips_challenge_monogame.Game1();
+using var game = new Chips_challenge_monogame.CHIP();
 game.Run();
